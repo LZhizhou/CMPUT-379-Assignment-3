3a652fd7c50879ebc30fc15b357eb99b201efdeb
zhizhou <luzhizhou@hotmail.com>
2019-04-10 00:26:46 -0600

    final

--- run.py
+++ run.py
@@ -6,12 +6,11 @@ policy = ["FIFO", "LRU"]
 page_size = [512,4096,32768]
 tlb_size = [32,128,512]
 program = ["./heapsort", "./quicksort", "./mergesort" ]
-ins_size = ["10000","100000"]
+ins_size = ["1000", "10000", "100000"]
 i = [True, False]
-flush = [True, False]
 
 class mythread (threading.Thread):
-    def __init__(self, p , page, tlb, pro, ins, i, flush):
+    def __init__(self, p , page, tlb, pro, ins, i):
         threading.Thread.__init__(self)
         self.p = p
         self.page = page
@@ -19,53 +18,49 @@ class mythread (threading.Thread):
         self.pro = pro
         self.ins = ins
         self.i = i
-        self.flush = flush
 
     def run(self):
         threadLimiter.acquire()
-        try:
-            a = self.ins + " " + self.pro + ' ' + self.p + " page size: " + str(self.page) + " tlb size: " + str(self.tlb) + " i: " + str(
-                self.i) + " flush: " + str(self.flush)
-            sort = subprocess.Popen(['valgrind', '--tool=lackey', '--trace-mem=yes', str(self.pro), str(self.ins)]
-                                    , stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
 
-            # f = open(a, "w")
-            base = ['./valtlb379', '-p', '-i', str(self.p), str(self.page), str(self.tlb)]
-            if self.i:
-                base.append('-i')
-            if self.flush:
-                base.append('-f')
-                base.append('10000')
-            result = subprocess.Popen(base, stdin=sort.stdout, stdout=subprocess.PIPE)
-            threadLock.acquire()
-            print(a)
-            print(result.stdout.readline())
-            print(result.stdout.readline())
-            print(result.stdout.readline())
-            print("========================")
-            threadLock.release()
-        finally:
-            threadLimiter.release()
+        a = [self.ins , self.pro , self.p , str(self.page) , str(self.tlb) ,str(self.i)]
+        sort = subprocess.Popen(['valgrind', '--tool=lackey', '--trace-mem=yes', str(self.pro), str(self.ins)]
+                                , stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+
+        # f = open(a, "w")
+        base = ['./valtlb379', '-p', str(self.p), str(self.page), str(self.tlb)]
+        if self.i:
+            base.append('-i')
+        result = subprocess.Popen(base, stdin=sort.stdout, stdout=subprocess.PIPE)
+        threadLock.acquire()
+        result.stdout.readline()
+        #a.append(result.stdout.readline().decode("UTF-8").strip().split(" ")[0])
+        a.append(result.stdout.readline().decode("UTF-8").strip().split(" ")[0])
+        a.append(result.stdout.readline().decode("UTF-8").strip().split(" ")[0])
+        for i in range(len(a)):
+            print(a[i],end='')
+            if (i == len(a)-1):
+                print('')
+            else:
+                print(',',end='')
+        threadLock.release()
+
+        threadLimiter.release()
 
 
 
 
 if __name__ == "__main__":
+    print ("int_size,program,policy,page_size,tlb_size,i,miss,hit")
     threadLock = threading.Lock()
-    threadLimiter = threading.BoundedSemaphore(6)
-    threads = []
-    maxThreads = 50
-    q = queue.Queue(6)
+    threadLimiter = threading.BoundedSemaphore(12)
     for p in policy:
         for page in page_size:
             for tlb in tlb_size:
                 for pro in program:
                     for ins in ins_size:
                         for i_or_not in i:
-                            for flush_or_not in flush:
-                                t = mythread(p,page,tlb, pro, ins, i_or_not, flush_or_not)
-                                t.start()
-                                threads.append(t)
+                            t = mythread(p,page,tlb, pro, ins, i_or_not)
+                            t.start()
 
 
 
